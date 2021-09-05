e4cfaffb503494a4112798b57fb3b542e5c6f5b5
aclar <a.clarke0307@gmail.com>
2021-09-04 21:07:16 -0400

    fix mistake

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+const PORT = process.env.PORT || 3001;
+
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const PORT = process.env.PORT || 3001;
 const app = express();
 const addedNotes = require('./bd/bd.json');
 
@@ -10,41 +11,40 @@ app.use(express.json());
 app.use(express.static('public'));
 
 function newNote(body, noteArray){
-    const addNote = body;
+    const newNote = body;
 if(!Array.isArray(noteArray))
    noteArray =[];
 if(noteArray.length === 0)
    noteArray.push(0);
    body.id = noteArray[0];
    noteArray[0]++;
-   
-   noteArray.push(addNote);
+
+   noteArray.push(newNote);
    fs.writeFileSync(
        path.join(__dirname, './db/db.json'),
        JSON.stringify(noteArray, null, 2)
    );
-   return addNote;
+   return newNote;
 }
 
+app.post('/api/notes', (req, res)=> {
+    const newNote = newNote(req.body, addedNotes);
+    res.json(newNote);
+})
 
 app.get('/api/notes', (req, res)=> {
     res.json(addedNotes.slice(1));
 });
-app.get('/',(req, res)=>{
+app.get('/',(req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
-app.get('/',(req, res)=>{
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
-app.get('*',(req, res)=>{
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-
-app.post('/api/notes', (req, res)=> {
-    const addNote = newNote(req.body, addedNotes);
-    res.json(addNote);
-})
 
 app.listen(PORT, ()=>{
     console.log(`API server now on port ${PORT}!`);
