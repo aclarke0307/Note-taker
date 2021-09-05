f9d8b9f1ca64488e74c3fa679165613b2a5ae325
aclar <a.clarke0307@gmail.com>
2021-09-04 20:03:45 -0400

    add note function and post function

--- server.js
+++ server.js
@@ -2,8 +2,25 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const addedNotes = require('./bd/bd.json');
 
+function newNote(body, noteArray){
+    const addNote = body;
+if(!Array.isArray(noteArray))
+   noteArray =[];
 
+   noteArray.push(addNote);
+   fs.writeFileSync(
+       path.join(__dirname, './db/db.json'),
+       JSON.stringify(noteArray, null, 2)
+   );
+   return addNote;
+}
+
+app.post('/api/notes', (req, res)=> {
+    const addNote = newNote(req.body, addedNotes);
+    res.json(addNote);
+})
 
 
 
